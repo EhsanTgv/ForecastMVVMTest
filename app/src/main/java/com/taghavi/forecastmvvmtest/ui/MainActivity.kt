17c61186ad1fc54398402f02a7f53dec1272f692
EhsanTgv <EhsanTgv@gmail.com>
2021-09-21 01:09:34 +0430

    setup fragments and navigation

---
+++ app/src/main/java/com/taghavi/forecastmvvmtest/ui/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.taghavi.forecastmvvmtest.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.taghavi.forecastmvvmtest.R
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(navController)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
+    }
+}
---
+++ app/src/main/java/com/taghavi/forecastmvvmtest/ui/weather/future/detail/FutureDetailsWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.taghavi.forecastmvvmtest.ui.weather.future.detail
+
+import androidx.lifecycle.ViewModel
+
+class FutureDetailsWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
