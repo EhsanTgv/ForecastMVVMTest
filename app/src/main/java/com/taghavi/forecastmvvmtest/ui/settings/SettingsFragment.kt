78487fc57ccf0ad2b6d876907a02c65b74897181
EhsanTgv <EhsanTgv@gmail.com>
2021-10-31 16:30:00 +0330

    Implement Settings & preferences

--- app/src/main/java/com/taghavi/forecastmvvmtest/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/taghavi/forecastmvvmtest/ui/settings/SettingsFragment.kt
@@ -1,9 +1,19 @@
 package com.taghavi.forecastmvvmtest.ui.settings
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
+import com.taghavi.forecastmvvmtest.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.preferences)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        (activity as? AppCompatActivity)?.supportActionBar?.title = "Settings"
+        (activity as? AppCompatActivity)?.supportActionBar?.subtitle = null
+    }
 }
